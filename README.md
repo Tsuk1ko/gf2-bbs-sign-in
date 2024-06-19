d12223151d7daec84fa0eb2e1a4f3b8a1fe0d988
神代綺凛 <i@loli.best>
2024-06-19 11:15:47 +0800

    fix: default auto exchange

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## Secrets / ENV
 
-- `BBS_AUTH`  
+- `BBS_AUTH` (必须)  
   前往[官方社区](https://gf2-bbs.sunborngame.com/)，登录后 <kbd>F12</kbd> Network 随便找个 API 请求 Request Headers 里拿到 Authorization，可多账号，以逗号 `,` 分隔
-- `AUTO_EXCHANGE`  
+- `AUTO_EXCHANGE` (可选)  
   自动兑换道具，按优先级顺序排列，以逗号 `,` 分隔，默认为 `基原信息核,萨狄斯金,情报拼图`
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import { GF2BBSClient } from './client';
 
-const { BBS_AUTH, AUTO_EXCHANGE = '基原信息核,萨狄斯金,情报拼图' } =
-  process.env;
-
-console.log('自动兑换:', AUTO_EXCHANGE);
+const { BBS_AUTH, AUTO_EXCHANGE } = process.env;
 
 if (!BBS_AUTH) {
   console.error('No BBS_AUTH');
   process.exit(1);
 }
+
+const autoExchange = (AUTO_EXCHANGE || '基原信息核,萨狄斯金,情报拼图').split(',');
+
+console.log('自动兑换:', autoExchange);
 
 let hasError = false;
 
@@ -26,9 +27,7 @@ for (const [i, auth] of BBS_AUTH.split(',').entries()) {
       console.log('今日已签到');
     } else {
       const { item, count, exp, score } = await client.signIn();
-      console.log(
-        `签到成功，获得【${item}*${count}】，经验+${exp}，积分+${score}`
-      );
+      console.log(`签到成功，获得【${item}*${count}】，经验+${exp}，积分+${score}`);
     }
 
     const info = await client.getUserInfo();
@@ -36,14 +35,11 @@ for (const [i, auth] of BBS_AUTH.split(',').entries()) {
 
     const itemMap = Object.fromEntries(
       (await client.getExchangeList())
-        .filter(
-          ({ exchange_count, max_exchange_count }) =>
-            exchange_count < max_exchange_count
-        )
-        .map((item) => [item.item_name, item])
+        .filter(({ exchange_count, max_exchange_count }) => exchange_count < max_exchange_count)
+        .map(item => [item.item_name, item]),
     );
 
-    for (const name of AUTO_EXCHANGE.split(',')) {
+    for (const name of autoExchange) {
       const item = itemMap[name];
       if (!item) continue;
       const count = await client.exchangeIfCan(item);
